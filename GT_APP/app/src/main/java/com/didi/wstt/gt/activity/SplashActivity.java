d06dfa8e776257c1860dcb8d07ba91c1a580371d
mezzsy <mezzsy@qq.com>
2020-10-14 21:27:13 +0800

    修改SplashActivity

--- GT_APP/app/src/main/java/com/didi/wstt/gt/activity/SplashActivity.java
+++ GT_APP/app/src/main/java/com/didi/wstt/gt/activity/SplashActivity.java
@@ -8,14 +8,14 @@
  * in and to the previous version of Tencent GT (including any and all copies thereof)
  * shall be owned and retained by Tencent and subject to the license under the
  * Tencent GT End User License Agreement (http://gt.qq.com/wp-content/EULA_EN.html).
- * 
+ *
  * Copyright (C) 2015 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://opensource.org/licenses/MIT
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed
  * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -31,60 +31,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ * 首页
+ */
 public class SplashActivity extends Activity {
 
-	private static SplashActivity INSTANCE;
-	
-	public static SplashActivity getInstance() {
-		return INSTANCE;
-	}
+    private static SplashActivity INSTANCE;
 
-	/**
-	 * 是否是首次进入欢迎页
-	 */
-	public static boolean sIsFirstTimeEnter = true;
+    public static SplashActivity getInstance() {
+        return INSTANCE;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		INSTANCE = this;
-		// 已启动
-		if(GTApp.getGTRunStatus()){
-			if(!GTMainActivity.isActived){
-				Intent intent = new Intent(SplashActivity.this, GTMainActivity.class);
-				startActivity(intent);
-			}
-			finish();
-		}
-		// 启动
-		else
-		{
-			GTApp.setGTRunStatus(true);
+    /**
+     * 是否是首次进入欢迎页
+     */
+    public static boolean sIsFirstTimeEnter = true;
 
-			// 新启动进闪屏页2s
-			if (sIsFirstTimeEnter) {
-				
-				// 显示闪屏
-				setContentView(R.layout.gt_splash);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        INSTANCE = this;
+        if (GTApp.getGTRunStatus()) {// 已启动
+            if (!GTMainActivity.isActived) {
+                Intent intent = new Intent(SplashActivity.this, GTMainActivity.class);
+                startActivity(intent);
+            }
+            finish();
+        } else {// 启动
+            GTApp.setGTRunStatus(true);
 
-				// 开启入口service 开启远程服务service
-				GTEntrance.GTopen(getApplicationContext());
-
-				new Handler().postDelayed(new Runnable() {
-					public void run() {
-						Intent intent = new Intent(SplashActivity.this,
-								GTMainActivity.class);
-						startActivity(intent);
-						SplashActivity.this.finish();
-					}
-				}, 2000);
-
-				sIsFirstTimeEnter = false;
-			} else {
-				Intent intent = new Intent(this, GTMainActivity.class);
-				startActivity(intent);
-				this.finish();
-			}
-		}
-	}
+            // 新启动进闪屏页1s
+            if (sIsFirstTimeEnter) {
+                // 显示闪屏
+                setContentView(R.layout.gt_splash);
+                // 开启入口service 开启远程服务service
+                GTEntrance.GTopen(getApplicationContext());
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this,
+                                GTMainActivity.class);
+                        startActivity(intent);
+                        SplashActivity.this.finish();
+                    }
+                }, 1000);
+                sIsFirstTimeEnter = false;
+            } else {
+                Intent intent = new Intent(this, GTMainActivity.class);
+                startActivity(intent);
+                this.finish();
+            }
+        }
+    }
 }
